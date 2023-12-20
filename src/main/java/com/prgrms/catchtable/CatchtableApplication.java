d4b83a3a0cd1ab22ca0aae0a19aa7627853533e0
hs12 <loveanna1228@gmail.com>
2023-12-20 13:51:03 +0900

    init : 프로젝트 생성

---
+++ src/main/java/com/prgrms/catchtable/CatchtableApplication.java
@@ -0,0 +1,13 @@
+package com.prgrms.catchtable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchtableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatchtableApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prgrms/catchtable/CatchtableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prgrms.catchtable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatchtableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
