c56bce6dacafb47bcbf25fc2feef4624a178bd56
dlswns2480 <dlswns2480@naver.com>
2023-12-23 03:05:32 +0900

    feat : 예약시간 선점 api 틀 구현

---
+++ src/main/java/com/prgrms/catchtable/reservation/controller/ReservationController.java
@@ -0,0 +1,25 @@
+package com.prgrms.catchtable.reservation.controller;
+
+import com.prgrms.catchtable.reservation.dto.request.CreateResercationRequest;
+import com.prgrms.catchtable.reservation.dto.response.CreateReservationResponse;
+import com.prgrms.catchtable.reservation.service.ReservationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/reservations")
+@RequiredArgsConstructor
+public class ReservationController {
+
+    private final ReservationService reservationService;
+
+    @GetMapping("/{shopId}")
+    public CreateReservationResponse createReservationResponse(
+        @PathVariable("shopId") Long shopId,
+        CreateResercationRequest request) {
+        return null;
+    }
+}
