c34397d7da851e541a987f9ac4a21bf06c8e1b4f
dlswns2480 <dlswns2480@naver.com>
2023-12-26 01:20:40 +0900

    refactor : 예약 등록 요청 dto 네이밍 오타 수정

--- src/main/java/com/prgrms/catchtable/reservation/dto/request/CreateReservationRequest.java
+++ src/main/java/com/prgrms/catchtable/reservation/dto/request/CreateReservationRequest.java
@@ -2,6 +2,6 @@ package com.prgrms.catchtable.reservation.dto.request;
 
 import java.time.LocalDateTime;
 
-public record CreateResercationRequest(LocalDateTime date, int peopleCount) {
+public record CreateReservationRequest(LocalDateTime date, int peopleCount) {
 
 }
