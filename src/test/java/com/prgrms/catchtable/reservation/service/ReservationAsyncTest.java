826fb02ea1376f533e7bc51df5f3bcb3376212cd
dlswns2480 <dlswns2480@naver.com>
2023-12-26 02:00:33 +0900

    feat : 예약 선점 비동기 메소드 SpringbootTest로 변경

--- src/test/java/com/prgrms/catchtable/reservation/service/ReservationAsyncTest.java
+++ src/test/java/com/prgrms/catchtable/reservation/service/ReservationAsyncTest.java
@@ -1,33 +1,28 @@
 package com.prgrms.catchtable.reservation.service;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import com.prgrms.catchtable.common.data.reservation.ReservationData;
 import com.prgrms.catchtable.reservation.domain.ReservationTime;
-import com.prgrms.catchtable.reservation.repository.ReservationRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ReservationAsyncTest {
 
-    @Mock
-    private ReservationRepository reservationRepository;
-    @InjectMocks
+    @Autowired
     private ReservationAsync reservationAsync;
 
     @Test
     @DisplayName("예약 선점을 실행한 후 7분이 지나기 전 후로 선점여부 값이 알맞게 바뀌어야 한다.")
-    void preOccupiedAsync(){
+    void preOccupiedAsync() {
         ReservationTime reservationTime = ReservationData.getReservationTime();
         reservationAsync.setPreOcuppied(reservationTime);
+        reservationTime.reversePreOccupied();
 
         assertThat(reservationTime.isPreOccupied()).isTrue();
 
@@ -36,7 +31,7 @@ class ReservationAsyncTest {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-
+        reservationTime.reversePreOccupied();
         assertThat(reservationTime.isPreOccupied()).isFalse();
     }
 }
