7a12e9411753bd969dc4bf09a5373630bd25705d
dlswns2480 <dlswns2480@naver.com>
2023-12-25 15:20:55 +0900

    feat : ErrorCode 상태코드 필드 삭제

--- src/main/java/com/prgrms/catchtable/common/exception/ErrorCode.java
+++ src/main/java/com/prgrms/catchtable/common/exception/ErrorCode.java
@@ -2,13 +2,11 @@ package com.prgrms.catchtable.common.exception;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
-    NOT_EXIST_MEMBER(HttpStatus.BAD_REQUEST, "존재하지 않는 아이디입니다.");
+    NOT_EXIST_MEMBER("존재하지 않는 아이디입니다.");
 
-    private final HttpStatus httpStatus;
     private final String message;
 }
