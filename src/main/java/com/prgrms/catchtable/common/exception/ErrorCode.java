cf1de2d74faf1282cec5d79314a55ceb36f776fb
dlswns2480 <dlswns2480@naver.com>
2023-12-27 14:48:22 +0900

    feat : 예약 선점에 필요한 에러코드 추가

--- src/main/java/com/prgrms/catchtable/common/exception/ErrorCode.java
+++ src/main/java/com/prgrms/catchtable/common/exception/ErrorCode.java
@@ -6,7 +6,11 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
-    NOT_EXIST_MEMBER("존재하지 않는 아이디입니다.");
+    NOT_EXIST_MEMBER("존재하지 않는 아이디입니다."),
+    IS_PRE_OCCUPIED("이미 타인에게 선점권이 있는 예약시간입니다."),
+    IS_OCCUPIED("이미 예약된 시간입니다."),
+    NOT_EXIST_SHOP("존재하지 않는 매장입니다."),
+    NOT_EXIST_TIME("존재하지 않는 예약 시간입니다.");
 
     private final String message;
 }
