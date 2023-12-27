17a180d20673881a72440aa0f2170c94c5ab71cd
hs12 <loveanna1228@gmail.com>
2023-12-28 03:04:13 +0900

    feat : 대기 repository 함수 선언 및 test 코드 작성

---
+++ src/test/java/com/prgrms/catchtable/member/MemberFixture.java
@@ -0,0 +1,18 @@
+package com.prgrms.catchtable.member;
+
+import com.prgrms.catchtable.member.domain.Gender;
+import com.prgrms.catchtable.member.domain.Member;
+import com.prgrms.catchtable.shop.domain.Shop;
+import java.time.LocalDate;
+
+public class MemberFixture {
+    public static Member member(String name) {
+        return Member.builder()
+            .name(name)
+            .phoneNumber("010-1111-1111")
+            .gender(Gender.FEMALE)
+            .dateBirth(LocalDate.parse("2008-12-18"))
+            .notification_activated(true)
+            .build();
+    }
+}
