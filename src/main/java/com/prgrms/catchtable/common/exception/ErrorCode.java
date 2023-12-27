bb6ee06de62334a82f6b7848d89285be06af4370
hs12 <loveanna1228@gmail.com>
2023-12-28 03:15:57 +0900

    feat : 대기 생성 비즈니스 로직 구현

--- src/main/java/com/prgrms/catchtable/common/exception/ErrorCode.java
+++ src/main/java/com/prgrms/catchtable/common/exception/ErrorCode.java
@@ -2,11 +2,17 @@ package com.prgrms.catchtable.common.exception;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
-    NOT_EXIST_MEMBER("존재하지 않는 아이디입니다.");
+    NOT_EXIST_MEMBER("존재하지 않는 아이디입니다."),
+    NOT_EXIST_SHOP("존재하지 않는 가게 아이디입니다."),
 
+    ALREADY_CANCELED_WAITING("이미 웨이팅을 취소하였습니다."),
+    EXISTING_MEMBER_WAITING("이미 회원이 웨이팅 중인 가게가 존재합니다."),
+    SHOP_NOT_RUNNING("가게가 영업시간이 아닙니다."),
+    INTERNAL_SERVER_ERROR("내부 서버 오류입니다.");
     private final String message;
 }
---
+++ src/main/java/com/prgrms/catchtable/waiting/service/WaitingService.java
@@ -0,0 +1,59 @@
+package com.prgrms.catchtable.waiting.service;
+
+import static com.prgrms.catchtable.common.exception.ErrorCode.EXISTING_MEMBER_WAITING;
+import static com.prgrms.catchtable.waiting.domain.WaitingStatus.PROGRESS;
+
+import com.prgrms.catchtable.common.exception.custom.BadRequestCustomException;
+import com.prgrms.catchtable.member.domain.Member;
+import com.prgrms.catchtable.shop.domain.Shop;
+import com.prgrms.catchtable.waiting.domain.Waiting;
+import com.prgrms.catchtable.waiting.dto.CreateWaitingRequest;
+import com.prgrms.catchtable.waiting.dto.CreateWaitingResponse;
+import com.prgrms.catchtable.waiting.dto.WaitingMapper;
+import com.prgrms.catchtable.waiting.facade.WaitingFacade;
+import com.prgrms.catchtable.waiting.repository.WaitingRepository;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class WaitingService {
+    private final LocalDateTime START_DATE_TIME = LocalDateTime.of(LocalDate.now(), LocalTime.of(0,0,0));
+    private final LocalDateTime END_DATE_TIME = LocalDateTime.of(LocalDate.now(), LocalTime.of(23,59,59));
+    private final WaitingRepository waitingRepository;
+    private final WaitingFacade waitingFacade;
+    public CreateWaitingResponse createWaiting(Long shopId, CreateWaitingRequest request) {
+        // 연관 엔티티 조회
+        Member member = waitingFacade.getMemberEntity(1L); //member, shop 아이디 임시 1L
+        Shop shop = waitingFacade.getShopEntity(shopId);
+
+        // shop 영업 중인지 검증
+        shop.validateIfShopOpened(LocalTime.now());
+
+        // 기존 waiting이 있는지 검증
+        validateIfMemberWaitingExists(member);
+
+        // 대기 번호 생성
+        int waitingNumber = (waitingRepository.countByShopAndStatusAndCreatedAtBetween(shop,
+            PROGRESS, START_DATE_TIME, END_DATE_TIME)).intValue()+1;
+
+        // 대기 순서 생성
+        int waitingOrder = (waitingRepository.countByShopAndCreatedAtBetween(shop,
+            START_DATE_TIME, END_DATE_TIME)).intValue()+1;
+
+        // waiting 저장
+        Waiting waiting = WaitingMapper.toWaiting(request, waitingNumber, waitingOrder, member, shop);
+        Waiting savedWaiting = waitingRepository.save(waiting);
+
+        return WaitingMapper.toCreateWaitingResponse(savedWaiting);
+    }
+
+    private void validateIfMemberWaitingExists(Member member) {
+        if (waitingRepository.existsByMember(member)){
+            throw new BadRequestCustomException(EXISTING_MEMBER_WAITING);
+        }
+    }
+}
