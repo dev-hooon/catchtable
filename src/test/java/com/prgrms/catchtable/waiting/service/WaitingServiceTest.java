08da02b37002b3c5658f6549ee5cdd7c99066c6c
hs12 <loveanna1228@gmail.com>
2023-12-28 17:50:20 +0900

    fix : 웨이팅 서비스 테스트 오류 해결

--- src/test/java/com/prgrms/catchtable/waiting/service/WaitingServiceTest.java
+++ src/test/java/com/prgrms/catchtable/waiting/service/WaitingServiceTest.java
@@ -2,22 +2,23 @@ package com.prgrms.catchtable.waiting.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
 
-import com.prgrms.catchtable.member.MemberFixture;
 import com.prgrms.catchtable.member.domain.Member;
 import com.prgrms.catchtable.shop.domain.Shop;
-import com.prgrms.catchtable.shop.fixture.ShopFixture;
 import com.prgrms.catchtable.waiting.domain.Waiting;
 import com.prgrms.catchtable.waiting.dto.CreateWaitingRequest;
 import com.prgrms.catchtable.waiting.dto.CreateWaitingResponse;
 import com.prgrms.catchtable.waiting.facade.WaitingFacade;
 import com.prgrms.catchtable.waiting.repository.WaitingRepository;
+import java.time.LocalTime;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
@@ -36,8 +37,8 @@ class WaitingServiceTest {
         CreateWaitingRequest request = CreateWaitingRequest.builder()
             .peopleCount(2)
             .build();
-        Shop shop = ShopFixture.shop();
-        Member member = MemberFixture.member("member1");
+        Shop shop = mock(Shop.class);
+        Member member = mock(Member.class);
         Waiting waiting = Waiting.builder()
             .member(member)
             .shop(shop)
@@ -45,10 +46,11 @@ class WaitingServiceTest {
             .waitingOrder(1)
             .peopleCount(2)
             .build();
+        doNothing().when(shop).validateIfShopOpened(any(LocalTime.class));
         given(waitingFacade.getShopEntity(1L)).willReturn(shop);
         given(waitingFacade.getMemberEntity(1L)).willReturn(member);
         given(waitingRepository.existsByMember(member)).willReturn(false);
-        given(waitingRepository.save(Mockito.any(Waiting.class))).willReturn(waiting);
+        given(waitingRepository.save(any(Waiting.class))).willReturn(waiting);
 
         //when
         CreateWaitingResponse response = waitingService.createWaiting(1L, request);
