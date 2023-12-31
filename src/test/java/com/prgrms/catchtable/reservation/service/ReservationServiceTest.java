d0e7cad958e1790d7b0e6805ec42f3ecd2b0ab03
dlswns2480 <dlswns2480@naver.com>
2023-12-31 23:47:29 +0900

    feat : 최종 예약 등록 검증 로직 테스트

--- src/test/java/com/prgrms/catchtable/reservation/service/ReservationServiceTest.java
+++ src/test/java/com/prgrms/catchtable/reservation/service/ReservationServiceTest.java
@@ -1,14 +1,19 @@
 package com.prgrms.catchtable.reservation.service;
 
+import static com.prgrms.catchtable.reservation.domain.ReservationStatus.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import com.prgrms.catchtable.common.data.reservation.ReservationData;
 import com.prgrms.catchtable.common.exception.custom.BadRequestCustomException;
+import com.prgrms.catchtable.reservation.domain.Reservation;
+import com.prgrms.catchtable.reservation.domain.ReservationStatus;
 import com.prgrms.catchtable.reservation.domain.ReservationTime;
 import com.prgrms.catchtable.reservation.dto.request.CreateReservationRequest;
+import com.prgrms.catchtable.reservation.repository.ReservationRepository;
 import com.prgrms.catchtable.reservation.repository.ReservationTimeRepository;
 import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
@@ -21,7 +26,8 @@ import org.springframework.test.util.ReflectionTestUtils;
 
 @ExtendWith(MockitoExtension.class)
 class ReservationServiceTest {
-
+    @Mock
+    ReservationRepository reservationRepository;
     @Mock
     private ReservationTimeRepository reservationTimeRepository;
     @InjectMocks
@@ -66,4 +72,42 @@ class ReservationServiceTest {
 
 
     }
+
+    @Test
+    @DisplayName("최종예약을 등록할 때 예약시간이 비었으면 성공적으로 예약 등록을 완료한다.")
+    void registerReservation(){
+        ReservationTime reservationTime = ReservationData.getReservationTimePreOccupied();
+        CreateReservationRequest request = ReservationData.getCreateReservationRequest();
+        Reservation reservation = Reservation.builder()
+            .status(COMPLETED)
+            .peopleCount(request.peopleCount())
+            .shop(reservationTime.getShop())
+            .reservationTime(reservationTime)
+            .build();
+
+        when(reservationTimeRepository.findByIdWithShop(any(Long.class))).thenReturn(
+            Optional.of(reservationTime));
+        when(reservationRepository.save(any(Reservation.class))).thenReturn(reservation);
+        Reservation findReservation = reservationService.validateReservationAndSaveIsEmpty(request);
+
+        assertAll(
+            () -> assertThat(findReservation.getReservationTime().getTime()).isEqualTo(reservationTime.getTime()),
+            () -> assertThat(findReservation.getPeopleCount()).isEqualTo(request.peopleCount()),
+            () -> assertThat(findReservation.getStatus()).isEqualTo(COMPLETED)
+        );
+    }
+    @Test
+    @DisplayName("최종예약을 등록할 때 타인이 이미 예약한 경우 예외가 발생한다.")
+    void registerReservationAlreadyOccupied() {
+        ReservationTime reservationTime = ReservationData.getReservationTimePreOccupied();
+        CreateReservationRequest request = ReservationData.getCreateReservationRequest();
+
+        reservationTime.reverseOccupied();
+        when(reservationTimeRepository.findByIdWithShop(any(Long.class))).thenReturn(
+            Optional.of(reservationTime));
+
+        assertThrows(BadRequestCustomException.class,
+            () -> reservationService.validateReservationAndSaveIsEmpty(request));
+    }
+
 }
