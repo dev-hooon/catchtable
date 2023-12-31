2a6980648535427a7f995778b2cf707bd0a3bbe8
dlswns2480 <dlswns2480@naver.com>
2023-12-31 23:46:53 +0900

    feat : 최종 예약 등록 시 예약이 비었는지 검증 후 예약 객체 생성하는 로직 추가

--- src/main/java/com/prgrms/catchtable/reservation/service/ReservationService.java
+++ src/main/java/com/prgrms/catchtable/reservation/service/ReservationService.java
@@ -1,12 +1,16 @@
 package com.prgrms.catchtable.reservation.service;
 
+import static com.prgrms.catchtable.common.exception.ErrorCode.ALREADY_OCCUPIED_RESERVATION_TIME;
 import static com.prgrms.catchtable.common.exception.ErrorCode.ALREADY_PREOCCUPIED_RESERVATION_TIME;
 import static com.prgrms.catchtable.common.exception.ErrorCode.NOT_EXIST_TIME;
+import static com.prgrms.catchtable.reservation.domain.ReservationStatus.COMPLETED;
 
 import com.prgrms.catchtable.common.exception.custom.BadRequestCustomException;
 import com.prgrms.catchtable.common.exception.custom.NotFoundCustomException;
+import com.prgrms.catchtable.reservation.domain.Reservation;
 import com.prgrms.catchtable.reservation.domain.ReservationTime;
 import com.prgrms.catchtable.reservation.dto.request.CreateReservationRequest;
+import com.prgrms.catchtable.reservation.repository.ReservationRepository;
 import com.prgrms.catchtable.reservation.repository.ReservationTimeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,6 +21,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class ReservationService {
 
     private final ReservationTimeRepository reservationTimeRepository;
+    private final ReservationRepository reservationRepository;
 
     @Transactional
     public ReservationTime validateReservationAndSave(CreateReservationRequest request) {
@@ -29,5 +34,25 @@ public class ReservationService {
         }
 
         return reservationTime;
+    }
+    
+    @Transactional
+    public Reservation validateReservationAndSaveIsEmpty(CreateReservationRequest request){
+        ReservationTime reservationTime = reservationTimeRepository.findByIdWithShop(
+            request.reservationTimeId()).
+            orElseThrow(() -> new NotFoundCustomException(NOT_EXIST_TIME));
+        
+        if(reservationTime.isOccupied()){
+            throw new BadRequestCustomException(ALREADY_OCCUPIED_RESERVATION_TIME);
+        }
+
+        Reservation reservation = Reservation.builder()
+            .status(COMPLETED)
+            .peopleCount(request.peopleCount())
+            .shop(reservationTime.getShop())
+            .reservationTime(reservationTime)
+            .build();
+        Reservation savedReservation = reservationRepository.save(reservation);
+        return savedReservation;
     }
 }
