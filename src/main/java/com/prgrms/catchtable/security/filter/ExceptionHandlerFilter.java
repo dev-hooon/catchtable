59f7b36c04f114050fa5e95ee9864014bee075fb
kkangh00n <kig2454@gmail.com>
2024-01-02 11:18:29 +0900

    feat : security 내 예외 처리 필터 구현

---
+++ src/main/java/com/prgrms/catchtable/security/filter/ExceptionHandlerFilter.java
@@ -0,0 +1,47 @@
+package com.prgrms.catchtable.security.filter;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.prgrms.catchtable.common.exception.ErrorResponse;
+import com.prgrms.catchtable.common.exception.custom.BadRequestCustomException;
+import com.prgrms.catchtable.common.exception.custom.NotFoundCustomException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.GenericFilter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ExceptionHandlerFilter extends GenericFilter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+        throws IOException, ServletException {
+
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+
+        try {
+            chain.doFilter(request, response);
+        } catch (BadRequestCustomException be) {
+            httpServletResponse.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            sendJson(httpServletResponse, toJson(new ErrorResponse(be.getErrorCode())));
+        } catch (NotFoundCustomException ne) {
+            httpServletResponse.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            sendJson(httpServletResponse, toJson(new ErrorResponse(ne.getErrorCode())));
+        }
+    }
+
+    private String toJson(ErrorResponse response) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.writeValueAsString(response);
+    }
+
+    private void sendJson(HttpServletResponse response, String resultJson) throws IOException {
+        response.setContentType("application/json;charset=UTF-8");
+        response.setContentLength(resultJson.getBytes().length);
+        response.getWriter().write(resultJson);
+    }
+}
