00072b8661b01f8780238e6cf2289c419500363c
hs12 <loveanna1228@gmail.com>
2024-01-04 17:30:56 +0900

    refactor : response 통일을 위한 파일명 수정

---
+++ src/main/java/com/prgrms/catchtable/waiting/dto/WaitingResponse.java
@@ -0,0 +1,15 @@
+package com.prgrms.catchtable.waiting.dto;
+
+import lombok.Builder;
+
+@Builder
+public record WaitingResponse(
+    Long createdWaitingId,
+    Long shopId,
+    String shopName,
+    int peopleCount,
+    int waitingNumber,
+    int waitingOrder
+) {
+
+}
