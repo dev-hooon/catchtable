97a831d7cc302451309caa0d2c0eea2ffd922df9
dlswns2480 <dlswns2480@naver.com>
2024-01-04 19:05:10 +0900

    feat : 예약 예제 데이터 추가

--- src/test/java/com/prgrms/catchtable/reservation/fixture/ReservationFixture.java
+++ src/test/java/com/prgrms/catchtable/reservation/fixture/ReservationFixture.java
@@ -6,6 +6,7 @@ import com.prgrms.catchtable.common.data.shop.ShopData;
 import com.prgrms.catchtable.reservation.domain.Reservation;
 import com.prgrms.catchtable.reservation.domain.ReservationTime;
 import com.prgrms.catchtable.reservation.dto.request.CreateReservationRequest;
+import com.prgrms.catchtable.reservation.dto.request.ModifyReservationRequest;
 import com.prgrms.catchtable.shop.domain.Shop;
 import java.time.LocalDateTime;
 import org.springframework.test.util.ReflectionTestUtils;
@@ -53,6 +54,33 @@ public class ReservationFixture {
             .reservationTimeId(1L)
             .peopleCount(4)
             .build();
+    }
+
+    public static ModifyReservationRequest getModifyReservationRequest(Long reservationTimeId){
+        return ModifyReservationRequest.builder()
+            .reservationTimeId(reservationTimeId)
+            .peopleCount(2)
+            .build();
+    }
+
+
+    public static ReservationTime getAnotherReservationTimeNotPreOccupied() {
+        ReservationTime reservationTime = ReservationTime.builder()
+            .time(LocalDateTime.of(2024, 11, 30, 19, 30))
+            .build();
+        reservationTime.insertShop(ShopData.getShop());
+        return reservationTime;
+    }
+
+    public static ReservationTime getReservationTimeOccupied() {
+        ReservationTime reservationTime = ReservationTime.builder()
+            .time(LocalDateTime.of(2024, 12, 31, 19, 30))
+            .build();
+        Shop shop = ShopData.getShop();
+        ReflectionTestUtils.setField(shop, "id", 1L);
+        reservationTime.insertShop(shop);
+        reservationTime.reverseOccupied();
+        return reservationTime;
     }
 
 }
