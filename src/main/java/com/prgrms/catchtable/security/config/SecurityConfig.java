f65e117fbc823f02632a48ee8d8d538390d50135
kkangh00n <kig2454@gmail.com>
2024-01-04 14:50:57 +0900

    fix : 설정 파일의 어노테이션 수정

--- src/main/java/com/prgrms/catchtable/security/config/SecurityConfig.java
+++ src/main/java/com/prgrms/catchtable/security/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.prgrms.catchtable.security.filter.ExceptionHandlerFilter;
 import com.prgrms.catchtable.security.service.CustomOAuth2SuccessHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -14,7 +15,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.stereotype.Component;
 
-@Component
+@Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
