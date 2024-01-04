5b67aeaa4bfc40619b3a928f0d568206aabbd576
kkangh00n <kig2454@gmail.com>
2024-01-04 17:58:04 +0900

    feat : Owner 회원가입 API

---
+++ src/main/java/com/prgrms/catchtable/owner/controller/OwnerController.java
@@ -0,0 +1,29 @@
+package com.prgrms.catchtable.owner.controller;
+
+import com.prgrms.catchtable.owner.dto.request.JoinOwnerRequest;
+import com.prgrms.catchtable.owner.dto.response.JoinOwnerResponse;
+import com.prgrms.catchtable.owner.service.OwnerService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/owners")
+public class OwnerController {
+
+    private final OwnerService ownerService;
+
+    @PostMapping("/join")
+    public ResponseEntity<JoinOwnerResponse> join(@Valid @RequestBody JoinOwnerRequest joinOwnerRequest){
+        JoinOwnerResponse joinOwnerResponse = ownerService.joinOwner(joinOwnerRequest);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(joinOwnerResponse);
+    }
+
+}
