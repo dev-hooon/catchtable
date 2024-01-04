d2aab0431987bedd7c5341aa7de1ff49c87b055b
dlswns2480 <dlswns2480@naver.com>
2024-01-04 15:56:37 +0900

    feat : 예약시간과 매장 아이디로 예약시간 읽어오는 쿼리 작성

--- src/main/java/com/prgrms/catchtable/reservation/repository/ReservationTimeRepository.java
+++ src/main/java/com/prgrms/catchtable/reservation/repository/ReservationTimeRepository.java
@@ -1,6 +1,8 @@
 package com.prgrms.catchtable.reservation.repository;
 
 import com.prgrms.catchtable.reservation.domain.ReservationTime;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,4 +12,8 @@ public interface ReservationTimeRepository extends JpaRepository<ReservationTime
 
     @Query("select rt from ReservationTime rt join fetch rt.shop s where rt.id = :id")
     Optional<ReservationTime> findByIdWithShop(@Param("id") Long id);
+
+    //매장에 해당 예약시간이 있는지 읽어오는 작업
+    @Query("select rt from ReservationTime rt where rt.id = :reservationTimeId and rt.shop.id = :shopId")
+    Optional<ReservationTime> findByIdAndShoId(@Param("reservationTimeId") Long reservationTimeId, @Param("shopId") Long shopId);
 }
