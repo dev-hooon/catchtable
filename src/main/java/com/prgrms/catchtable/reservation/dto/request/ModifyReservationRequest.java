72cf83e4521b3ec65f5926220e9dac0a9bd29fdd
dlswns2480 <dlswns2480@naver.com>
2024-01-04 19:04:35 +0900

    feat : 예약 수정 요청 dto 빌더패턴 적용

--- src/main/java/com/prgrms/catchtable/reservation/dto/request/ModifyReservationRequest.java
+++ src/main/java/com/prgrms/catchtable/reservation/dto/request/ModifyReservationRequest.java
@@ -1,10 +1,8 @@
 package com.prgrms.catchtable.reservation.dto.request;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonFormat.Shape;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
+import lombok.Builder;
 
+@Builder
 public record ModifyReservationRequest(Long reservationTimeId,
                                        int peopleCount) {
 
