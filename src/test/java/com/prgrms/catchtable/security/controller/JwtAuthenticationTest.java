16e8fb6470e75dc52b51b1c4335b65a986285257
kkangh00n <kig2454@gmail.com>
2024-01-04 11:11:59 +0900

    refactor : BaseIntegrationTest 미상속

--- src/test/java/com/prgrms/catchtable/security/controller/JwtAuthenticationTest.java
+++ src/test/java/com/prgrms/catchtable/security/controller/JwtAuthenticationTest.java
@@ -3,7 +3,6 @@ package com.prgrms.catchtable.security.controller;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.prgrms.catchtable.common.base.BaseIntegrationTest;
 import com.prgrms.catchtable.jwt.filter.JwtAuthenticationFilter;
 import com.prgrms.catchtable.jwt.provider.JwtTokenProvider;
 import com.prgrms.catchtable.jwt.service.RefreshTokenService;
@@ -16,12 +15,19 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
+@SpringBootTest
+@Transactional
+@AutoConfigureMockMvc
 @WebAppConfiguration
-class JwtAuthenticationTest extends BaseIntegrationTest {
+class JwtAuthenticationTest{
 
     @Autowired
     WebApplicationContext webApplicationContext;
@@ -36,6 +42,7 @@ class JwtAuthenticationTest extends BaseIntegrationTest {
     RefreshTokenService refreshTokenService;
     @Autowired
     JwtTokenProvider jwtTokenProvider;
+    MockMvc mockMvc;
 
     Member loginMember;
     String email = "abc1234@gmail.com";
