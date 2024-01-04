71573cdae86e5be6a49770a370fda6fc2d208f32
dlswns2480 <dlswns2480@naver.com>
2024-01-04 22:54:17 +0900

    test : 예약 수정 api 구현

--- src/main/java/com/prgrms/catchtable/reservation/controller/ReservationController.java
+++ src/main/java/com/prgrms/catchtable/reservation/controller/ReservationController.java
@@ -1,10 +1,15 @@
 package com.prgrms.catchtable.reservation.controller;
 
 import com.prgrms.catchtable.reservation.dto.request.CreateReservationRequest;
+import com.prgrms.catchtable.reservation.dto.request.ModifyReservationRequest;
 import com.prgrms.catchtable.reservation.dto.response.CreateReservationResponse;
+import com.prgrms.catchtable.reservation.dto.response.ModifyReservationResponse;
 import com.prgrms.catchtable.reservation.service.ReservationService;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,5 +32,11 @@ public class ReservationController {
     public ResponseEntity<CreateReservationResponse> registerReservation(
         @RequestBody CreateReservationRequest request) {
         return ResponseEntity.ok(reservationService.registerReservation(request));
+    }
+
+    @PatchMapping("/{reservationId}")
+    public ResponseEntity<ModifyReservationResponse> modifyReservation(@PathVariable("reservationId") Long reservationTimeId,
+        @RequestBody ModifyReservationRequest request){
+        return ResponseEntity.ok(reservationService.modifyReservation(reservationTimeId, request));
     }
 }
