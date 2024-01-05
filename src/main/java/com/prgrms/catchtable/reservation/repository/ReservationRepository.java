f3236683ab8bf7fa090fc452935a25df88864f5a
dlswns2480 <dlswns2480@naver.com>
2024-01-06 02:45:00 +0900

    feat : Owner 예약정보 조회용 쿼리 작성

--- src/main/java/com/prgrms/catchtable/reservation/repository/ReservationRepository.java
+++ src/main/java/com/prgrms/catchtable/reservation/repository/ReservationRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
@@ -15,6 +16,12 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
 
     @Query("select r from Reservation r "
         + "join fetch r.reservationTime rt "
-        + "join fetch rt.shop")
-    Optional<Reservation> findByIdWithReservationTimeAndShop(Long reservationId);
+        + "join fetch rt.shop "
+        + "where r.id = :reservationId")
+    Optional<Reservation> findByIdWithReservationTimeAndShop(@Param("reservationId") Long reservationId);
+    @Query("select r from Reservation r "
+        + "join fetch r.reservationTime rt "
+        + "join fetch rt.shop s "
+        + "where s.id = :shopId")
+    List<Reservation> findAllWithReservationTimeAndShopByShopId(@Param("shopId") Long shopId);
 }
