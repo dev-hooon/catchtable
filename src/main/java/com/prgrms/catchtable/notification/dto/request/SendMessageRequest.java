fa47bf878bb019e304fe49a39e159b43583bce59
dlswns2480 <dlswns2480@naver.com>
2024-01-07 17:30:22 +0900

    feat : 알림 전송 요청 dto 생성

---
+++ src/main/java/com/prgrms/catchtable/notification/dto/request/SendMessageRequest.java
@@ -0,0 +1,7 @@
+package com.prgrms.catchtable.notification.dto.request;
+
+import com.prgrms.catchtable.common.NotificationContent;
+
+public record SendMessageRequest(NotificationContent content) {
+
+}
