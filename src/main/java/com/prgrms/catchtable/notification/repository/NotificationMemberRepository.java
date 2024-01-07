d28e062a6d62d9ab0bd6951d99b5121500b7296a
dlswns2480 <dlswns2480@naver.com>
2024-01-07 17:29:41 +0900

    feat : 개인 알림 조회용 쿼리

--- src/main/java/com/prgrms/catchtable/notification/repository/NotificationMemberRepository.java
+++ src/main/java/com/prgrms/catchtable/notification/repository/NotificationMemberRepository.java
@@ -1,8 +1,10 @@
 package com.prgrms.catchtable.notification.repository;
 
+import com.prgrms.catchtable.member.domain.Member;
 import com.prgrms.catchtable.notification.domain.NotificationMember;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationMemberRepository extends JpaRepository<NotificationMember, Long> {
-
+    Optional<NotificationMember> findByMember(Member member);
 }
