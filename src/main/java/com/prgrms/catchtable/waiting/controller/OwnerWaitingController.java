7f7f417224be3ac8e043fa6c914cfb681cf1384b
hs12 <loveanna1228@gmail.com>
2024-01-08 22:01:25 +0900

    feat : 웨이팅 입장 컨트롤러 추가

--- src/main/java/com/prgrms/catchtable/waiting/controller/OwnerWaitingController.java
+++ src/main/java/com/prgrms/catchtable/waiting/controller/OwnerWaitingController.java
@@ -1,10 +1,12 @@
 package com.prgrms.catchtable.waiting.controller;
 
 import com.prgrms.catchtable.waiting.dto.response.OwnerWaitingListResponse;
+import com.prgrms.catchtable.waiting.dto.response.OwnerWaitingResponse;
 import com.prgrms.catchtable.waiting.service.OwnerWaitingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +22,12 @@ public class OwnerWaitingController {
     public ResponseEntity<OwnerWaitingListResponse> getOwnerAllWaiting(
         @PathVariable("ownerId") Long ownerId) {
         OwnerWaitingListResponse response = ownerWaitingService.getOwnerAllWaiting(ownerId);
+        return ResponseEntity.ok(response);
+    }
+
+    @PatchMapping("/{ownerId}")
+    public ResponseEntity<OwnerWaitingResponse> entryWaiting(@PathVariable("ownerId") Long ownerId){
+        OwnerWaitingResponse response = ownerWaitingService.entryWaiting(ownerId);
         return ResponseEntity.ok(response);
     }
 }
