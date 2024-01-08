d754df39645a5409102d618bb178f85dbcfb08bc
kkangh00n <kig2454@gmail.com>
2024-01-08 19:33:34 +0900

    feat : 현재 로그인 정보 조회 어노테이션 추가 (@LogIn)

---
+++ src/main/java/com/prgrms/catchtable/common/login/LogIn.java
@@ -0,0 +1,12 @@
+package com.prgrms.catchtable.common.login;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LogIn {
+
+}
---
+++ src/main/java/com/prgrms/catchtable/common/login/WebConfig.java
@@ -0,0 +1,16 @@
+package com.prgrms.catchtable.common.login;
+
+import java.util.List;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers){
+        resolvers.add(new LogInArgumentResolver());
+    }
+
+}
