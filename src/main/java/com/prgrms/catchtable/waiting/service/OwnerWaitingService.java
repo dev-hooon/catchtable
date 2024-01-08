89dbdc0abe82ddcfed24f7bf221a8996915be9dd
hs12 <loveanna1228@gmail.com>
2024-01-08 21:50:39 +0900

    feat : 웨이팅 입장 로직 작성

--- src/main/java/com/prgrms/catchtable/waiting/service/OwnerWaitingService.java
+++ src/main/java/com/prgrms/catchtable/waiting/service/OwnerWaitingService.java
@@ -1,13 +1,17 @@
 package com.prgrms.catchtable.waiting.service;
 
 import static com.prgrms.catchtable.common.exception.ErrorCode.NOT_EXIST_OWNER;
+import static com.prgrms.catchtable.common.exception.ErrorCode.WAITING_DOES_NOT_EXIST;
 import static com.prgrms.catchtable.waiting.dto.WaitingMapper.toOwnerWaitingListResponse;
+import static com.prgrms.catchtable.waiting.dto.WaitingMapper.toOwnerWaitingResponse;
 
 import com.prgrms.catchtable.common.exception.custom.BadRequestCustomException;
+import com.prgrms.catchtable.common.exception.custom.NotFoundCustomException;
 import com.prgrms.catchtable.owner.domain.Owner;
 import com.prgrms.catchtable.owner.repository.OwnerRepository;
 import com.prgrms.catchtable.waiting.domain.Waiting;
 import com.prgrms.catchtable.waiting.dto.response.OwnerWaitingListResponse;
+import com.prgrms.catchtable.waiting.dto.response.OwnerWaitingResponse;
 import com.prgrms.catchtable.waiting.repository.WaitingRepository;
 import com.prgrms.catchtable.waiting.repository.waitingline.WaitingLineRepository;
 import java.util.List;
@@ -31,5 +35,16 @@ public class OwnerWaitingService {
             owner.getShop().getId());
         List<Waiting> waitings = waitingRepository.findByIds(waitingIds);
         return toOwnerWaitingListResponse(waitings);
+    }
+
+    @Transactional
+    public OwnerWaitingResponse entryWaiting(Long ownerId){
+        Owner owner = ownerRepository.findById(ownerId)
+            .orElseThrow(() -> new BadRequestCustomException(NOT_EXIST_OWNER));
+        Long enteredWaitingId = waitingLineRepository.entry(owner.getShop().getId());
+        Waiting waiting = waitingRepository.findById(enteredWaitingId)
+            .orElseThrow(() -> new NotFoundCustomException(WAITING_DOES_NOT_EXIST));
+        waiting.changeStatusCompleted();
+        return toOwnerWaitingResponse(waiting, 0L);
     }
 }
