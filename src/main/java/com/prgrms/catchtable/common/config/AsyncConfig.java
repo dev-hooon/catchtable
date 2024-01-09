9f8b5ccc757b305d0b84a4a458c26f4c6b272148
dlswns2480 <dlswns2480@naver.com>
2024-01-09 15:17:12 +0900

    feat : 비동기 config 추가

---
+++ src/main/java/com/prgrms/catchtable/common/config/AsyncConfig.java
@@ -0,0 +1,9 @@
+package com.prgrms.catchtable.common.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@org.springframework.scheduling.annotation.EnableAsync
+public class AsyncConfig {
+
+}
