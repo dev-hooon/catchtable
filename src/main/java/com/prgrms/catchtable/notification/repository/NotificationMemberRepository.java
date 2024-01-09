1f757eeb240c837b14988aa5d60b2316c09cf33c
dlswns2480 <dlswns2480@naver.com>
2024-01-09 15:18:50 +0900

    style : 알림 조회용 쿼리 자료형 List로 변경

--- src/main/java/com/prgrms/catchtable/notification/repository/NotificationMemberRepository.java
+++ src/main/java/com/prgrms/catchtable/notification/repository/NotificationMemberRepository.java
@@ -2,10 +2,11 @@ package com.prgrms.catchtable.notification.repository;
 
 import com.prgrms.catchtable.member.domain.Member;
 import com.prgrms.catchtable.notification.domain.NotificationMember;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationMemberRepository extends JpaRepository<NotificationMember, Long> {
 
-    Optional<NotificationMember> findByMember(Member member);
+    List<NotificationMember> findByMember(Member member);
 }
