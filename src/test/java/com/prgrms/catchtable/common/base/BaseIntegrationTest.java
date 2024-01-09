3df4196ff8b10bff7160e576930e214f37476025
dlswns2480 <dlswns2480@naver.com>
2024-01-09 13:34:10 +0900

    feat : BaseIntegration에 토큰 관련 필요의존성 추가

--- src/test/java/com/prgrms/catchtable/common/base/BaseIntegrationTest.java
+++ src/test/java/com/prgrms/catchtable/common/base/BaseIntegrationTest.java
@@ -2,16 +2,24 @@ package com.prgrms.catchtable.common.base;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.prgrms.catchtable.jwt.provider.JwtTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public abstract class BaseIntegrationTest {
 
+    @Autowired
+    public JwtTokenProvider jwtTokenProvider;
+
     public static ObjectMapper objectMapper = new ObjectMapper();
+
+    public HttpHeaders httpHeaders = new HttpHeaders();
     @Autowired
     public MockMvc mockMvc;
 
