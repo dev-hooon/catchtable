d93b70ce1341735a2a8163bb95110e2ff0e727bd
kkangh00n <kig2454@gmail.com>
2024-01-09 14:09:21 +0900

    fix : Category 객체 검증 메서드 추가

--- src/main/java/com/prgrms/catchtable/shop/domain/Category.java
+++ src/main/java/com/prgrms/catchtable/shop/domain/Category.java
@@ -1,5 +1,8 @@
 package com.prgrms.catchtable.shop.domain;
 
+import com.prgrms.catchtable.common.exception.ErrorCode;
+import com.prgrms.catchtable.common.exception.custom.BadRequestCustomException;
+import java.util.Arrays;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -11,6 +14,16 @@ public enum Category {
     CHINESE_FOOD("중식"),
     WESTERN_FOOD("양식");
 
-    private final String description;
+    private final String type;
 
+    public static Category of(String input){
+        return Arrays.stream(values())
+            .filter(category -> category.isEqual(input))
+            .findAny()
+            .orElseThrow(() -> new BadRequestCustomException(ErrorCode.INVALID_INPUT_TYPE));
+    }
+
+    private boolean isEqual(String input){
+        return type.equals(input);
+    }
 }
