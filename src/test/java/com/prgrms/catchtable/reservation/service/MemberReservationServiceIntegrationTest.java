25633e3d26ad0144cd0c6f75a8db99bc2a539433
dlswns2480 <dlswns2480@naver.com>
2024-01-09 13:32:24 +0900

    feat : 서비스에 예약과 회원 연관관계 매핑 테스트

--- src/test/java/com/prgrms/catchtable/reservation/service/MemberReservationServiceIntegrationTest.java
+++ src/test/java/com/prgrms/catchtable/reservation/service/MemberReservationServiceIntegrationTest.java
@@ -4,6 +4,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.prgrms.catchtable.common.data.shop.ShopData;
 import com.prgrms.catchtable.common.exception.custom.BadRequestCustomException;
+import com.prgrms.catchtable.member.MemberFixture;
+import com.prgrms.catchtable.member.domain.Member;
+import com.prgrms.catchtable.member.repository.MemberRepository;
 import com.prgrms.catchtable.reservation.domain.ReservationTime;
 import com.prgrms.catchtable.reservation.dto.request.CreateReservationRequest;
 import com.prgrms.catchtable.reservation.fixture.ReservationFixture;
@@ -33,10 +36,17 @@ public class MemberReservationServiceIntegrationTest {
     @Autowired
     private ShopRepository shopRepository;
 
+    @Autowired
+    private MemberRepository memberRepository;
+
     @BeforeEach
     void setUp() {
         Shop shop = ShopData.getShop();
         Shop savedShop = shopRepository.save(shop);
+
+        Member member = MemberFixture.member("dlswns661035@gmail.com");
+        memberRepository.save(member);
+
 
         ReservationTime reservationTime = ReservationFixture.getReservationTimeNotPreOccupied();
         reservationTime.insertShop(savedShop);
@@ -46,6 +56,7 @@ public class MemberReservationServiceIntegrationTest {
     @Test
     @DisplayName("동시에 요청이 들어오면 하나만 선점권이 true로 바뀌고 나머진 예외가 발생한다.")
     void concurrencyTest() throws InterruptedException {
+        Member member = memberRepository.findAll().get(0);
         AtomicInteger errorCount = new AtomicInteger(0);
 
         List<ReservationTime> all = reservationTimeRepository.findAll();
@@ -60,7 +71,7 @@ public class MemberReservationServiceIntegrationTest {
         for (int i = 0; i < threadCount; i++) {
             executorService.submit(() -> {
                 try {
-                    memberReservationService.preOccupyReservation(request);
+                    memberReservationService.preOccupyReservation(member, request);
                 } catch (BadRequestCustomException e) {
                     errorCount.incrementAndGet();
                 } finally {
