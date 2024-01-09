3750c7641dd4bdd795e3f9c77af9b8e0c56e40c7
dlswns2480 <dlswns2480@naver.com>
2024-01-09 16:05:40 +0900

    feat : 이벤트 리스너 인자 수정

--- src/main/java/com/prgrms/catchtable/common/notification/NotificationEvent.java
+++ src/main/java/com/prgrms/catchtable/common/notification/NotificationEvent.java
@@ -2,9 +2,9 @@ package com.prgrms.catchtable.common.notification;
 
 import static org.springframework.transaction.event.TransactionPhase.AFTER_COMMIT;
 
-import com.prgrms.catchtable.member.domain.Member;
+import com.prgrms.catchtable.notification.dto.request.SendMessageToMemberRequest;
+import com.prgrms.catchtable.notification.dto.request.SendMessageToOwnerRequest;
 import com.prgrms.catchtable.notification.service.NotificationService;
-import com.prgrms.catchtable.owner.domain.Owner;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
@@ -18,13 +18,13 @@ public class NotificationEvent {
 
     @Async
     @TransactionalEventListener(phase = AFTER_COMMIT) // 호출한쪽의 트랜잭션이 커밋 된 후 이벤트 발생
-    public void sendMessage(Member member, NotificationContent content) {
-        notificationService.sendMessageAndSave(member, content);
+    public void sendMessage(SendMessageToMemberRequest request) {
+        notificationService.sendMessageAndSave(request.member(), request.content());
     }
 
     @Async
     @TransactionalEventListener(phase = AFTER_COMMIT) // 호출한쪽의 트랜잭션이 커밋 된 후 이벤트 발생
-    public void sendMessage(Owner owner, NotificationContent content) {
-        notificationService.sendMessageAndSave(owner, content);
+    public void sendMessage(SendMessageToOwnerRequest request) {
+        notificationService.sendMessageAndSave(request.owner(), request.content());
     }
 }
