6037600cdec3023faaa134c0d7237e106292534e
hs12 <loveanna1228@gmail.com>
2024-01-10 18:47:57 +0900

    feat : RestDocs Config 의존성 추가 및 member header 공통 함수 추가

--- src/test/java/com/prgrms/catchtable/common/restdocs/RestDocsSupport.java
+++ src/test/java/com/prgrms/catchtable/common/restdocs/RestDocsSupport.java
@@ -1,17 +1,23 @@
 package com.prgrms.catchtable.common.restdocs;
 
+import static com.prgrms.catchtable.common.Role.MEMBER;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.prgrms.catchtable.jwt.provider.JwtTokenProvider;
+import com.prgrms.catchtable.jwt.token.Token;
+import com.prgrms.catchtable.member.domain.Member;
 import jakarta.servlet.ServletException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpHeaders;
 import org.springframework.mock.web.MockFilterConfig;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
 import org.springframework.security.config.BeanIds;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
@@ -21,12 +27,16 @@ import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.filter.DelegatingFilterProxy;
 
 @ExtendWith(RestDocumentationExtension.class)
+@Import(RestDocsConfig.class)
 @SpringBootTest
 public abstract class RestDocsSupport {
 
     protected MockMvc mockMvc;
     @Autowired
     protected ObjectMapper objectMapper;
+
+    @Autowired
+    protected RestDocumentationResultHandler restDocs;
     protected HttpHeaders httpHeaders = new HttpHeaders();
 
     @Autowired
@@ -43,11 +53,19 @@ public abstract class RestDocsSupport {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
             .apply(
                 MockMvcRestDocumentation.documentationConfiguration(provider))  // rest docs 설정 주입
+            .alwaysDo(restDocs)
             .alwaysDo(MockMvcResultHandlers.print())
             .addFilters(
                 new CharacterEncodingFilter("UTF-8", true),
                 delegateProxyFilter
             )
             .build();
+    }
+
+    public HttpHeaders getHttpHeaders(Member member) {
+        Token token = jwtTokenProvider.createToken(member.getEmail(), MEMBER);
+        httpHeaders.add("AccessToken", token.getAccessToken());
+        httpHeaders.add("RefreshToken", token.getRefreshToken());
+        return httpHeaders;
     }
 }
