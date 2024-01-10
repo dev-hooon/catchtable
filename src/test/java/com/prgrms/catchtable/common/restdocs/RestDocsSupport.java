7ce4b29a6e8de1f9a4403bc3f1ed67b24c5cc707
hs12 <loveanna1228@gmail.com>
2024-01-10 17:57:56 +0900

    feat : Security Security 사용하도록 RestDocsSupport setUp 설정

--- src/test/java/com/prgrms/catchtable/common/restdocs/RestDocsSupport.java
+++ src/test/java/com/prgrms/catchtable/common/restdocs/RestDocsSupport.java
@@ -1,28 +1,53 @@
 package com.prgrms.catchtable.common.restdocs;
 
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.prgrms.catchtable.jwt.provider.JwtTokenProvider;
+import jakarta.servlet.ServletException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpHeaders;
+import org.springframework.mock.web.MockFilterConfig;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.security.config.BeanIds;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.filter.DelegatingFilterProxy;
 
 @ExtendWith(RestDocumentationExtension.class)
+@SpringBootTest
 public abstract class RestDocsSupport {
 
     protected MockMvc mockMvc;
-    protected ObjectMapper objectMapper = new ObjectMapper();
+    @Autowired
+    protected ObjectMapper objectMapper;
+    protected HttpHeaders httpHeaders = new HttpHeaders();
 
+    @Autowired
+    protected JwtTokenProvider jwtTokenProvider;
 
     @BeforeEach
-    void setUp(RestDocumentationContextProvider provider) {
-        this.mockMvc = MockMvcBuilders.standaloneSetup(initController())
-            .apply(documentationConfiguration(provider))
+    void setUp(final WebApplicationContext context,
+        final RestDocumentationContextProvider provider) throws ServletException {
+        DelegatingFilterProxy delegateProxyFilter = new DelegatingFilterProxy();
+        delegateProxyFilter.init(
+            new MockFilterConfig(context.getServletContext(), BeanIds.SPRING_SECURITY_FILTER_CHAIN));
+
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+            .apply(
+                MockMvcRestDocumentation.documentationConfiguration(provider))  // rest docs 설정 주입
+            .alwaysDo(MockMvcResultHandlers.print())
+            .addFilters(
+                new CharacterEncodingFilter("UTF-8", true),
+                delegateProxyFilter
+            )
             .build();
     }
-
-    protected abstract Object initController();
 }
