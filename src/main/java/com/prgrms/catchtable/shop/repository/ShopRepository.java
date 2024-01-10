a45eb51c341ba6f9a756b5946efeb49e5bd249f0
kkangh00n <kig2454@gmail.com>
2024-01-10 14:04:41 +0900

    fix : Shop 단일 조회 시, ReservationTime fetchJoin

--- src/main/java/com/prgrms/catchtable/shop/repository/ShopRepository.java
+++ src/main/java/com/prgrms/catchtable/shop/repository/ShopRepository.java
@@ -1,8 +1,12 @@
 package com.prgrms.catchtable.shop.repository;
 
 import com.prgrms.catchtable.shop.domain.Shop;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ShopRepository extends JpaRepository<Shop, Long>,  ShopRepositoryCustom{
-
+    @Query("select s from Shop s join fetch s.reservationTimeList where s.id = :id ")
+    Optional<Shop> findByIdWithTime(@Param("id") Long id);
 }
