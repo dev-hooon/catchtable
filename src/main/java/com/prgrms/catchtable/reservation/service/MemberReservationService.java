60b6bdb8d8c031da57d95f4dd1a9e3005d50df35
dlswns2480 <dlswns2480@naver.com>
2024-01-10 15:06:07 +0900

    feat : 예약 등록 시 알림 전송 로직 추가

--- src/main/java/com/prgrms/catchtable/reservation/service/MemberReservationService.java
+++ src/main/java/com/prgrms/catchtable/reservation/service/MemberReservationService.java
@@ -4,6 +4,7 @@ import static com.prgrms.catchtable.common.exception.ErrorCode.ALREADY_OCCUPIED_
 import static com.prgrms.catchtable.common.exception.ErrorCode.ALREADY_PREOCCUPIED_RESERVATION_TIME;
 import static com.prgrms.catchtable.common.exception.ErrorCode.NOT_EXIST_RESERVATION;
 import static com.prgrms.catchtable.common.exception.ErrorCode.NOT_EXIST_TIME;
+import static com.prgrms.catchtable.common.notification.NotificationContent.*;
 import static com.prgrms.catchtable.reservation.domain.ReservationStatus.CANCELLED;
 import static com.prgrms.catchtable.reservation.domain.ReservationStatus.COMPLETED;
 import static com.prgrms.catchtable.reservation.dto.mapper.ReservationMapper.toCancelReservationResponse;
@@ -13,7 +14,9 @@ import static java.lang.Boolean.FALSE;
 
 import com.prgrms.catchtable.common.exception.custom.BadRequestCustomException;
 import com.prgrms.catchtable.common.exception.custom.NotFoundCustomException;
+import com.prgrms.catchtable.common.notification.NotificationContent;
 import com.prgrms.catchtable.member.domain.Member;
+import com.prgrms.catchtable.notification.dto.request.SendMessageToMemberRequest;
 import com.prgrms.catchtable.reservation.domain.Reservation;
 import com.prgrms.catchtable.reservation.domain.ReservationTime;
 import com.prgrms.catchtable.reservation.dto.mapper.ReservationMapper;
@@ -29,6 +32,7 @@ import com.prgrms.catchtable.reservation.repository.ReservationTimeRepository;
 import com.prgrms.catchtable.shop.domain.Shop;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -40,6 +44,7 @@ public class MemberReservationService {
     private final ReservationRepository reservationRepository;
     private final ReservationAsync reservationAsync;
     private final ReservationLockRepository reservationLockRepository;
+    private final ApplicationEventPublisher publisher;
 
     @Transactional
     public CreateReservationResponse preOccupyReservation(Member member,
@@ -91,6 +96,13 @@ public class MemberReservationService {
             .member(member)
             .build();
         Reservation savedReservation = reservationRepository.save(reservation);
+
+        SendMessageToMemberRequest sendMessage = new SendMessageToMemberRequest(
+            member,
+            RESERVATION_COMPLETED
+        );
+
+        publisher.publishEvent(sendMessage);
         return toCreateReservationResponse(savedReservation);
     }
 
