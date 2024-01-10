59e286bebcfe616c7e210c15a04cf36febe0c95e
kkangh00n <kig2454@gmail.com>
2024-01-10 16:25:43 +0900

    fix : URL 수정

--- src/main/java/com/prgrms/catchtable/shop/controller/MemberShopController.java
+++ src/main/java/com/prgrms/catchtable/shop/controller/MemberShopController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/shops/")
+@RequestMapping("/shops")
 public class MemberShopController {
 
     private final MemberShopService memberShopService;
