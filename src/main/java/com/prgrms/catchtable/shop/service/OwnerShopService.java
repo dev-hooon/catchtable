a953ba0b32eb684e121af3545b8b4986df6aa0a1
kkangh00n <kig2454@gmail.com>
2024-01-12 16:33:19 +0900

    feat : 가게 update 쿼리

--- src/main/java/com/prgrms/catchtable/shop/service/OwnerShopService.java
+++ src/main/java/com/prgrms/catchtable/shop/service/OwnerShopService.java
@@ -1,6 +1,7 @@
 package com.prgrms.catchtable.shop.service;
 
 import com.prgrms.catchtable.owner.domain.Owner;
+import com.prgrms.catchtable.owner.repository.OwnerRepository;
 import com.prgrms.catchtable.reservation.domain.ReservationTime;
 import com.prgrms.catchtable.reservation.repository.ReservationTimeRepository;
 import com.prgrms.catchtable.shop.domain.Shop;
@@ -18,14 +19,19 @@ import org.springframework.transaction.annotation.Transactional;
 public class OwnerShopService {
 
     private final ShopRepository shopRepository;
+    private final OwnerRepository ownerRepository;
     private final ReservationTimeRepository reservationTimeRepository;
 
     @Transactional
     public RegisterShopResponse registerShop(RegisterShopRequest registerShopRequest, Owner owner) {
 
+        //가게 저장
         Shop registerShop = shopRepository.save(ShopMapper.toEntity(registerShopRequest));
+        //점주와 가게 관계 매핑
         owner.insertShop(registerShop);
+        ownerRepository.save(owner);
 
+        //예약 시간 엔티티 생성
         List<ReservationTime> reservationTimeList = registerShopRequest.reservationTimeRequestList()
             .stream()
             .map(time -> ReservationTime.builder()
@@ -33,6 +39,7 @@ public class OwnerShopService {
                 .build())
             .toList();
 
+        //예약 시간과 가게 관계 매핑
         for (ReservationTime reservationTime : reservationTimeList) {
             reservationTime.insertShop(registerShop);
             reservationTimeRepository.save(reservationTime);
