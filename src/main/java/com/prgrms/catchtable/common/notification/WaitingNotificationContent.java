cba57477b458fefb51ba81a5a41c8a2138768336
hs12 <loveanna1228@gmail.com>
2024-01-13 18:02:50 +0900

    feat : 알림 메세지 수정

--- src/main/java/com/prgrms/catchtable/common/notification/WaitingNotificationContent.java
+++ src/main/java/com/prgrms/catchtable/common/notification/WaitingNotificationContent.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Getter
 public enum WaitingNotificationContent {
-    REGISTERED("웨이팅이 등록되었습니다."),
+    REGISTERED("웨이팅이 등록되었습니다.\n내 순서 : %d번째"),
     CANCELED("웨이팅이 취소되었습니다."),
 
     THIRD_RANK("3번째 순서로 곧 입장하실 차례입니다.");
