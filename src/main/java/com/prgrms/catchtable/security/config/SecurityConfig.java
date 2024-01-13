1d449f19dbe7ff5292b729b5cb83d6bb6da9aeec
kkangh00n <kig2454@gmail.com>
2024-01-13 23:19:20 +0900

    feat : test를 위한 controller 삭제

--- src/main/java/com/prgrms/catchtable/security/config/SecurityConfig.java
+++ src/main/java/com/prgrms/catchtable/security/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
     private final ExceptionHandlerFilter exceptionHandlerFilter;
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
-    private final String[] memberWhiteList = {"/reservations/**", "/watings/**", "/testMember/**"};
+    private final String[] memberWhiteList = {"/reservations/**", "/watings/**"};
     private final String[] ownerWhiteList = {"/owners/shops/**", "/owners/waitings"};
 
     @Bean
