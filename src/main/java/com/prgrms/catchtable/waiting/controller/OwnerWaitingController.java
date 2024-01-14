2e8207f9d9bc852fbe166036f40a10a8aecf5ce9
kkangh00n <kig2454@gmail.com>
2024-01-15 00:05:23 +0900

    fix : URL Path 수정

--- src/main/java/com/prgrms/catchtable/waiting/controller/OwnerWaitingController.java
+++ src/main/java/com/prgrms/catchtable/waiting/controller/OwnerWaitingController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RequestMapping("owner/waitings")
+@RequestMapping("owners/waitings")
 @RestController
 public class OwnerWaitingController {
 
