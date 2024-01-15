4d60d7c3a46baa5319dd53a39cc7c8f80a4e5ab0
hs12 <loveanna1228@gmail.com>
2024-01-15 16:28:18 +0900

    feat : 웨이팅 알림 메세지 enum 필드 추가

--- src/main/java/com/prgrms/catchtable/common/notification/WaitingNotificationContent.java
+++ src/main/java/com/prgrms/catchtable/common/notification/WaitingNotificationContent.java
@@ -8,11 +8,13 @@ import lombok.RequiredArgsConstructor;
 public enum WaitingNotificationContent {
     MEMBER_CREATED("웨이팅이 등록되었습니다.\n내 순서 : %d번째"),
     MEMBER_CANCELED("웨이팅이 취소되었습니다."),
+    MEMBER_COMPLETED("가게에 입장 처리되었습니다."),
     MEMBER_ENTRY("웨이팅 입장할 차례입니다."),
     MEMBER_POSTPONED("웨이팅 순서를 %d번으로 미뤘습니다."),
     THIRD_RANK("3번째 순서로 곧 입장하실 차례입니다."),
+    FIRST_RANK("입장할 순서예요! 지금 매장으로 와주세요."),
     OWNER_CREATED("%d번째 웨이팅이 등록되었습니다."),
     OWNER_CANCELED("%d번째 웨이팅이 취소되었습니다.");
 
-    private final String message;
+    private final String content;
 }
