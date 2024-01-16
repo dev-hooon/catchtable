682c921d40f9d798b0d663d7b703dcef6decdb37
hs12 <loveanna1228@gmail.com>
2024-01-16 23:28:05 +0900

    feat : repository waitingCount 초기화 메서드 추가

--- src/main/java/com/prgrms/catchtable/shop/repository/ShopRepository.java
+++ src/main/java/com/prgrms/catchtable/shop/repository/ShopRepository.java
@@ -4,9 +4,17 @@ import com.prgrms.catchtable.shop.domain.Shop;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface ShopRepository extends JpaRepository<Shop, Long>, ShopRepositoryCustom {
 
     @EntityGraph(attributePaths = {"menuList"})
     Optional<Shop> findShopById(Long id);
+
+    @Transactional
+    @Modifying(clearAutomatically = true)
+    @Query("update Shop s set s.waitingCount = 0")
+    void initWaitingCount();
 }
